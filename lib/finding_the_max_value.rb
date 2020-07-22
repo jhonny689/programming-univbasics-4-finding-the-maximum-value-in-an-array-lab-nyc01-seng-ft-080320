80eb900406c981b7ca30109726a956e2483a1f32
Jhonny Chamoun <jhonny.chamoun@gmail.com>
2020-07-22 19:01:09 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,5 +1,6 @@
 def find_max_value(array)
   # Add your solution here
+  #first way of doing it is to set the max to the first element and while looping the array if a bigger value is found we set the max to that new value...
   #max= array[0]
   #for i in 1..array.size - 1 do
     #if array[i] > max
@@ -8,6 +9,8 @@ def find_max_value(array)
   #end
   #max
   #binding.pry
+  
+  #second way of doing it is to sort the array and return the last element
   array = array.sort
   array.last
 end
