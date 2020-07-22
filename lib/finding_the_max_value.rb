1c9002554619f76dbb25ebd9808d6418aaeacff9
Jhonny Chamoun <jhonny.chamoun@gmail.com>
2020-07-22 18:55:19 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,7 +1,7 @@
 def find_max_value(array)
   # Add your solution here
   max= array[0]
-  for i in 1..array.size do
+  for i in 1..array.size - 1 do
     binding.pry
     if array[i] > max
       max = array[i]
